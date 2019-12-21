904e35cf40d61bd98a5b6f7ce03b7e347cf80ceb
b117014 <b117014@iiit-bh.ac.in>
2019-12-21 16:09:47 +0530

    dispatch method on reducer

---
+++ src/store/action/auth/auth.js
@@ -0,0 +1,24 @@
+import {SETUSER} from '../../actionTypes'
+import {AsyncStorage} from 'react-native'
+
+
+export const newUser = (data)=>{
+    return{
+          type:SETUSER,
+          data
+    }
+}
+
+export const addUser = (data,types)=>{
+
+       return dispatch =>{
+           return new Promise((resolve,reject)=>{
+               axios.post(`https://renter-server.herokuapp.com/api/${types}`,data)
+                    .then(({token,...user})=>{
+                        AsyncStorage.setItem('jwtToken',JSON.stringify(token))
+                        dispatch(newUser(user))
+                        resolve();
+                    }).catch(err=>reject(err))
+           })
+       }
+}
