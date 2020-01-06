930115d707bf55431f61dcc8477cd9804703891b
b117014 <b117014@iiit-bh.ac.in>
2020-01-06 17:23:45 +0530

    add document file in rent app

---
+++ src/component/document/document.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View,TouchableOpacity,Text,Button,StyleSheet} from 'react-native';
+import {} from 'react-native-document-picker';
+
+
+class Document extends React.Component{
+        constructor(props){
+            super(props);
+
+        }
+
+        render(){
+            return(
+                
+            )
+        }
+}
