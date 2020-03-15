3b0208683bb42058714250ed3e0aef96afbeb0ff
b117014 <b117014@iiit-bh.ac.in>
2020-03-16 03:13:18 +0530

    style

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -11,16 +11,23 @@ import Dashboard from '../component/dashboard';
 import { RazorPay } from '../component/RazorPay/RazorPay';
 import RentPay from '../component/payment/rentPay';
 import Logout from '../component/reduxform/logout';
-
+import Ionicons from 'react-native-vector-icons/Ionicons'
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const MainContainer = (props)=>{
 
     return(
         <NavigationContainer>
-            <Drawer.Navigator drawerContentOptions={{activeTintColor:"#FA613F",itemStyle:{marginVertical:10,marginTop:20}}}>
-                <Drawer.Screen name="home" component={BottomNavigator} />
-                <Drawer.Screen name="profile" component = {Profile} />
+            <Drawer.Navigator drawerContentOptions={{activeTintColor:"#FA613F",itemStyle:{marginVertical:0,marginTop:20}}}
+             drawerType="front"
+             drawerStyle={{
+                
+             }}
+            >
+                <Drawer.Screen name="home" component={BottomNavigator} options={{drawerLabel:"Home",unmountOnBlur:true,drawerIcon:(tab)=><Ionicons name="ios-home" size={25} color={tab.color} />}}
+                
+                />
+                <Drawer.Screen name="profile" component = {Profile} options={{drawerLabel:"Profile",drawerIcon:(tab)=><Ionicons name="ios-person" size={25} color={tab.color} />}} />
                 <Drawer.Screen name="dashboard" component={Dashboard} />
                 <Drawer.Screen name="pay" component={RentPay} />
                 <Drawer.Screen name="logout" component={Logout} />
