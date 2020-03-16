5c9de32eb53971aa09f8262100c4249782a0c3dd
b117014 <b117014@iiit-bh.ac.in>
2020-03-16 23:53:36 +0530

    add transition in this app

--- src/container/BottomNavigator/BottomNavigator.js
+++ src/container/BottomNavigator/BottomNavigator.js
@@ -2,15 +2,17 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../../component/home/home';
 import Dashboard from '../../component/dashboard';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomNavigator = (props)=>{
 
     return(
-        <Tab.Navigator>
-            <Tab.Screen name="home" component={Home} />
-            <Tab.Screen name="dashboard" component={Dashboard} />
+        <Tab.Navigator shifting={true}>
+            <Tab.Screen name="home" component={Home} options={{tabBarLabel:"Home",tabBarColor:"#045BBC",tabBarIcon:(tab)=>(<MaterialCommunityIcons name="home" size={26} color={tab.color} />)}} />
+            <Tab.Screen name="dashboard" component={Dashboard} options={{tabBarLabel:"Dashboard",tabBarColor:"#FA5740",tabBarIcon:(tab)=>(<MaterialCommunityIcons name="view-dashboard" size={26} color={tab.color} />)}} />
         </Tab.Navigator>
     )
 }
